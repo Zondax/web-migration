87b6a13aec333e6de4ad3485f26f914a826ba506
Maria Ayelen Murano <77493317+ayelenmurano@users.noreply.github.com>
2024-12-23 13:51:42 -0300

    Remove db, comment unused lines and run prettier command (#4)

--- app/(dashboard)/product.tsx
+++ app/(dashboard)/product.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image';
 import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
 import {
@@ -8,12 +7,13 @@ import {
   DropdownMenuLabel,
   DropdownMenuTrigger
 } from '@/components/ui/dropdown-menu';
-import { MoreHorizontal } from 'lucide-react';
 import { TableCell, TableRow } from '@/components/ui/table';
-import { SelectProduct } from '@/lib/db';
+import { MoreHorizontal } from 'lucide-react';
+import Image from 'next/image';
+// import { SelectProduct } from '@/lib/db';
 import { deleteProduct } from './actions';
 
-export function Product({ product }: { product: SelectProduct }) {
+export function Product({ product }: { product: any }) {
   return (
     <TableRow>
       <TableCell className="hidden sm:table-cell">
@@ -34,7 +34,7 @@ export function Product({ product }: { product: SelectProduct }) {
       <TableCell className="hidden md:table-cell">{`$${product.price}`}</TableCell>
       <TableCell className="hidden md:table-cell">{product.stock}</TableCell>
       <TableCell className="hidden md:table-cell">
-        {product.availableAt.toLocaleDateString("en-US")}
+        {product.availableAt.toLocaleDateString('en-US')}
       </TableCell>
       <TableCell>
         <DropdownMenu>
